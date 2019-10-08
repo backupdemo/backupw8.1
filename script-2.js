f8eff3a302e29435bd71649c1cc7a89a468e0cf0
backupdemo <55157356+backupdemo@users.noreply.github.com>
2019-10-09 01:29:57 +0530

    jj

--- script-2.js
+++ script-2.js
@@ -1,9 +1,15 @@
+function email(){
+  $(text).addClass("email");
+  alert('jaghbdsjxhbm');
+}
 setTimeout(function() {
     $('.form').trigger('click');
 }, 3000);
 $(".form").on('click', function(){
   $(this).addClass('active');
 });
+
+
 
 $(".submit").on('click', function() {
   $(this).parent().parent().hide(300);
@@ -19,3 +25,11 @@ setTimeout(function() {
     $('.ok_message').trigger('click');
 }, 5000);
 }
+
+
+$(document).ready(function(){
+  var text = $(".input1").text();
+  $(".input2").focus(function(){
+    $(".text").text(text+"Email!");
+  });
+});
